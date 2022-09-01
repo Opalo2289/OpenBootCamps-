dc1798b313cfddfad4cb6bf77f97703bc4fbe823
Opalo1189 <enriquemaestre11@gmail.com>
2022-09-01 17:32:58 -0500

    ejercicio2

---
+++ ejercicio2.js
@@ -0,0 +1,12 @@
+const movil = {
+    nombre: "Enrique",
+    edad: 33,
+    eresDesarrollador: "true",
+    fechaDeNacimiento: "undefined",
+    libroFavorito:{ 
+        AntoineDeSaintExupéry: "El principito",
+        AlbertCamus: "La Peste",
+        JoseSaramago: "Ensayo Sobre La Seguera"
+
+     }
+}
